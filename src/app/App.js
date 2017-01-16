d84b995a06b77eab4c01126ae220fae4f5559ff9
createthink.net@gmail.com <41871879@qq.com>
2017-01-17 00:22:26 +0800

    精简首页

--- src/app/App.js
+++ src/app/App.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Logo from './Logo';
 import Table from '../table/Table';
 import './resources/app.css';
-import './resources/app.scss';
-import './resources/app.less';
-import simpsons from './resources/simpsons.jpg';
 import './resources/fontawesome/css/font-awesome.css';
 
 class App extends Component {
-  componentDidMount() {
-    //console.log('mount', this.props);
-  }
-  // 原始递增一个数
-  handleIncrement = () => {
-    let dispatch = this.props.dispatch;
-    //dispatch({type: 'INCREMENT'})
-    dispatch({type: 'INCREMENT_ASYNC'});
-  }
-  handleDecrement = () => {
-    let dispatch = this.props.dispatch;
-    dispatch({type: 'DECREMENT_ASYNC'});
-  }
   render() {
     return(
       <div>
-        <img src={simpsons} />
-        <div>dud ~~{this.props.num}~~ App</div>
-        <p><i className="fa fa-address-book-o fa-6"></i></p>
-        <Table />
-        <div>
-          <button onClick={this.handleIncrement}>INCREMENT 增加</button>
-          <button onClick={this.handleDecrement}>DECREMENT 减少</button>
+        <Logo />
+        <div className="">
+          <h3><i className="fa fa-hand-peace-o"></i>&nbsp;webpack react 脚手架</h3>
         </div>
+        <Table />
         {this.props.children || null}
       </div>
     );
